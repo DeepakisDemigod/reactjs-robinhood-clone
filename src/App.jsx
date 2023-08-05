a7fc7c2ea9cc456d26fb633c834a55187df7ecea
DeepakisDemigod <deepakthapa1423@gmail.com>
2023-08-05 21:28:43 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from "./Header.jsx"
+import Newsfeed from "./Newsfeed.jsx"
+import Stats from "./Stats.jsx"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        {/* Header */}
+        <div className="app__header">
+          <Header />
+        </div>
+        <div className="app__body">
+          <div className="app__container">
+            <Newsfeed />
+            {/* Stats */}
+            <Stats />
+          </div>
+        </div>
+        { /* Body*/}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
---
+++ src/Newsfeed.jsx
@@ -0,0 +1,16 @@
+import "./Newsfeed.css"
+
+function Newsfeed() {
+  return (
+    <div className="newsfeed">
+      <div className="newsfeed__container">
+        <div className="portfolio__value">
+          <h1>$114,656</h1>
+          <p>+44.63 (+0.04) Today</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Newsfeed
